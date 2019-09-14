a8aa30839c806c9386949ea394c5d1c16b51e598
Kenneth Spatta <kenyounot@gmail.com>
2019-09-14 03:26:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else
+      return false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else
+      return false
+    
+    end
   end
 
   def count_sentences
-
+    arr = self.split(/\?|\.|!/)
+    new = arr.reject(&:empty?).length
   end
+  
 end
+"Hello! How are you?? What are you doing today.".count_sentences
+
