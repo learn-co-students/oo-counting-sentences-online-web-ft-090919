3bd20764a052c9170913b545a6fe2e67b8ea8768
brunozanin <corporate.zanin@gmail.com>
2019-09-18 17:14:40 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 class String
-
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+      new_array = self.split(/[.?!]/).reject do |string|
+        string.empty?
+      end
+      return new_array.length
   end
 end
