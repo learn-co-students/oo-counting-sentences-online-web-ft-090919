2d5777a8774e91d76db5dd5f810a16f5d55541f7
Shaqqour M <mshaqqourm@gmail.com>
2019-09-19 21:51:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,48 @@
-require 'pry'
+require 'byebug'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    
+    if self.include?(".")
+      temp_count = self.split(".").length
+      if temp_count > 1
+        count += temp_count - 1
+      else
+        count += temp_count
+      end
+    end
+    
+    if self.include?("?")
+      temp_count = self.split("?").length
+      if temp_count > 1
+        count += temp_count - 1
+      else
+        count += temp_count
+      end
+    end
+    
+    if self.include?("!")
+      temp_count = self.split("!").length
+      if temp_count > 1
+        count += temp_count - 1
+      else
+        count += temp_count
+      end
+    end
+    count
   end
 end
